8d283a1d87c68fd0debfa273ae08285e0f481ed3
DRISS BOUMEDIENE <driss308@hotmail.fr>
2023-01-15 19:00:01 +0100

    quete30 done

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DefaultController extends AbstractController
 {
@@ -12,5 +13,12 @@ class DefaultController extends AbstractController
     public function index(): Response
     {
         return $this->render('/index.html.twig');
+    }
+
+    public function navbarTop(CategoryRepository $categoryRepository): Response
+    {
+        return $this->render('/_navbartop.html.twig', [
+            'categories' => $categoryRepository->findBy([], ['id' => 'DESC'])
+        ]);
     }
 }
